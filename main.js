70ed09dcf48e91da174c0b34b0b0e51a9e30d85d
Timothy Andrew Gass <timothyagass@gmail.com>
2015-10-22 16:29:50 -0500

    cleaned some code

--- main.js
+++ main.js
@@ -16,23 +16,40 @@ function quotient(num1, num2){
   return (num1 / num2);
 };
 
+function eval(){
+  if (!isNaN(document.getElementById("num_one").value)) {
+    num1 = Number(document.getElementById("num_one").value);
+}
+ else {
+   alert("I can only calculate numbers!");
+ }
+
+  if (!isNaN(document.getElementById("num_two").value)) {
+    num2 = Number(document.getElementById("num_two").value);
+}
+else {
+  alert("I can only calculate numbers!");
+}
+};
+
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("add").addEventListener("click", function(){
-       if (!isNaN(document.getElementById("num_one").value)) {
-         num1 = Number(document.getElementById("num_one").value);
-     }
-      else {
-        alert("we can only add numbers!");
-      }
-
-       if (!isNaN(document.getElementById("num_two").value)) {
-         num2 = Number(document.getElementById("num_two").value);
-     }
-     else {
-       alert("we can only add numbers!");
-     }
-
+    eval();
     document.getElementById('answer').innerHTML = sum(num1, num2).toString();
   });
 
+  document.getElementById("subtract").addEventListener("click", function(){
+    eval();
+    document.getElementById('answer').innerHTML = difference(num1, num2).toString();
+  });
+
+  document.getElementById("multiply").addEventListener("click", function(){
+    eval();
+    document.getElementById('answer').innerHTML = product(num1, num2).toString();
+  });
+
+  document.getElementById("divide").addEventListener("click", function(){
+    eval();
+    document.getElementById('answer').innerHTML = quotient(num1, num2).toString();
+  });
 });
